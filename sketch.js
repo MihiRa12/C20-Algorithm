656ef46156230f8a5dc4232823d0204942aafab2
MihiRa12 <75037926+MihiRa12@users.noreply.github.com>
2020-12-16 12:49:17 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+//collision detection algorithm
+
+/*var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 200, 50, 80);
+  fixedRect.shapeColor = "blue";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 400, 80, 30);
+  movingRect.shapeColor = "yellow";
+  movingRect.debug = true;
+}
+
+function draw() {
+  background("black");  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  console.log(movingRect.x-fixedRect.x);
+
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    fixedRect.shapeColor = "red";
+    movingRect.shapeColor = "red";
+  }
+  else{
+    fixedRect.shapeColor = "blue";
+    movingRect.shapeColor = "yellow";
+  }
+
+  drawSprites();
+}*/
+
+
+
+//bounceOff algorithm
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "blue";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800, 80, 30);
+  movingRect.shapeColor = "yellow";
+  movingRect.debug = true;
+  fixedRect.velocityY = 5;
+  movingRect.velocityY = -5;
+  
+}
+
+function draw() {
+  background("black");  
+  console.log(movingRect.x-fixedRect.x);
+
+  if(movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2){
+      movingRect.velocityX = movingRect.velocityX * (-1);
+      fixedRect.velocityX = fixedRect.velocityX * (-1);
+
+    }
+    if(movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+      movingRect.velocityY = movingRect.velocityY * (-1);
+      fixedRect.velocityY = fixedRect.velocityY * (-1);
+  }
+
+  drawSprites();
+}
